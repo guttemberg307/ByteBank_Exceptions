41c4e7af60654d42580a9970325ef02a694d8e47
guttemberg307 <guto2041@hotmail.com>
2022-10-17 15:12:19 -0300

    Bloco finally

---
+++ ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivo
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            throw new FileNotFoundException(); //execeção definida no.NET quando não é encontrado um arquivo
+
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+            //throw new IOException();// execeção definida no .NET onde tem imput e output/ entrada e saida 
+
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ByteBank
 {
-   
+
     class Program
     {
         static void Main(string[] args)
-        
+        {
+            CarregarContas();
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas() //metodo de carregar contas 
+        {
+            LeitorDeArquivo leitor = null; // leitor recebe o resultado nulo 
+            try
+            {
+                leitor = new LeitorDeArquivo("contasl.txt"); // inscia do leitor de arquivos com o arquivo contas.txt para leitura 
+
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Exceção do tipo IOException capturada e tratada!");
+            }
+            finally// Finally vai ser executanto sempre aconteça uma exceção ou não.
+            {
+                if (leitor != null) // se for diferente de nulo fechara autmaticamente caso não seja nulo a exceção sera tratada pois não foi encontrato o arquivo
+                {
+                    leitor.Fechar();
+                }
+            }
+        }
+        private static void TestaInnerException()
         {
             try
             {
@@ -19,7 +51,7 @@ namespace ByteBank
 
                 conta1.Transferir(10000, conta2);
             }
-            catch(OperacaoFinanceiraException e)
+            catch (OperacaoFinanceiraException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
@@ -29,16 +61,13 @@ namespace ByteBank
                 Console.WriteLine(e.InnerException.Message); // lembrando que so colocamos em evidencia a InnerExecption somente para visualização
                 Console.WriteLine(e.InnerException.StackTrace); // poque o cliente so ira ver Operação nao realizada
             }
-
-            Console.WriteLine("Execução finalizada. Tecle enter para sair");
-            Console.ReadLine();
         }
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
-           // TestaDivisao(2);
-           TestaDivisao(0);
+            // TestaDivisao(2);
+            TestaDivisao(0);
         }
 
         public static void TestaDivisao(int divisor)
@@ -46,7 +75,7 @@ namespace ByteBank
 
             int resultado = Dividir(10, divisor);
             Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-           
+
         }
 
         private static int Dividir(int numero, int divisor)
@@ -57,11 +86,11 @@ namespace ByteBank
             }
             catch
             {
-                Console.WriteLine("Execeção com numero = " + numero +"e divisor = " + divisor);
+                Console.WriteLine("Execeção com numero = " + numero + "e divisor = " + divisor);
                 throw; // throw vai laçar a execeção para a proxima, lançado os resultados dos valores reais de numero e divisor 
             }
-         
-           
+
+
         }
     }
 }
